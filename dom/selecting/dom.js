5201530e7bdbf80ba934ad98f979b2a9088c7bad
Josh Archer <jarcher@greenriver.edu>
2026-02-11 11:15:15 -0800

    Added examples of DOM interactions

---
+++ dom/selecting/dom.js
@@ -0,0 +1,23 @@
+
+//select multiple elements
+const elems = document.querySelectorAll("ul ul > li");
+console.log(elems);
+
+// const first = elems[0];
+// first.style.color = "green";
+// first.textContent = "Hello";
+
+for (let i = 0; i < elems.length; i++) {
+    elems[i].style.color = "green";
+}
+
+for (let elem of elems) {
+    elem.style.textTransform = "lowercase";
+}
+
+//target the science fictions movies and change the "text"
+const horror = document.querySelector("#favorite");
+//horror.innerHTML = "<strong>Hello</strong> world!";
+//horror.textContent = "<strong>Hello</strong> world!";
+const maliciousContent = "<script>window.location='https://www.google.com'</script>";
+horror.innerHTML = maliciousContent;
